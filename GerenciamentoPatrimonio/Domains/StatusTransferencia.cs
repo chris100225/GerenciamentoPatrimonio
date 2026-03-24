fc5cfbce051543965cd0bcce945a1e3e0434774e
CATARINA KONDOLIZA GARDENIA FAGUNDES DE MARTIN <42332992820@info.local>
2026-03-24 08:47:10 -0300

    :sparkles: Scaffold

---
+++ GerenciamentoPatrimonio/Domains/StatusTransferencia.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GerenciamentoPatrimonio.Domains;
+
+public partial class StatusTransferencia
+{
+    public Guid StatusTransferenciaID { get; set; }
+
+    public string NomeStatus { get; set; } = null!;
+}
